8531c9595d0cb13febd73dc0521e06eb5ee24cdc
M7a1s <yoarta87@gmail.com>
2025-09-17 11:40:22 +0330

    add scroll up

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { ThemeProvider } from "./context/ThemeProvider";
 import MainInfo from "./components/MainInfo";
 import MoreInfo from "./components/MoreInfo";
 import { Flip, ToastContainer } from "react-toastify";
+import ScrollBase from "./base/ScrollBase";
 
 const App = () => {
   return (
     <ThemeProvider>
+      <ScrollBase />
       <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick={false} rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="colored" transition={Flip} />
       <div className="fixed pointer-events-none inset-0 h-full w-full bg-[linear-gradient(to_right,#80808012_2px,transparent_2px),linear-gradient(to_bottom,#80808012_2px,transparent_2px)] bg-[size:52px_52px]" />
       <Header />
