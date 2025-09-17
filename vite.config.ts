cd8b2ac075e6b32925a9be86b6c50bebde2f0e11
M7a1s <yoarta87@gmail.com>
2025-09-17 11:24:58 +0330

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   envDir: "./",
+  base: "/Arta_Zanjani/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
